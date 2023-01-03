0c0ad4738cf34469a7c476938d298e0fb63ea859
MrsOwlya <olja-holmes@mail.ru>
2023-01-03 11:10:24 +0300

    added old code

---
+++ GatewayService/gateway/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('tickets', views.user_tickets_or_buy),
+    path('tickets/<uuid:ticketId>', views.user_ticket_info_or_cancel),
+    path('flights', views.flights_list),
+    path('me', views.user_info),
+    path('privilege', views.privilege_info)
+]
